0b2b07e8d76eeeb52c5f1773bc177ef8ee657b01
mehedy201 <mehedy643@gmail.com>
2026-04-04 17:57:39 +0600

    now user can see active page. active page menu color will be change

--- js_script.js
+++ js_script.js
@@ -1,3 +1,19 @@
+// Menu Active Class add JS 
+const currentPage = window.location.pathname.split("/").pop();
+
+document.querySelectorAll(".desktop_menu ul li a").forEach(link => {
+  let linkPage = link.getAttribute("href").split("/").pop();
+
+  // normalize (handle trailing slash)
+  if (linkPage === "") linkPage = "index.html";
+
+  if (currentPage === linkPage) {
+    link.parentElement.classList.add("active");
+  }
+});
+
+
+// Responsive Menu JS 
 const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile_menu");
 
